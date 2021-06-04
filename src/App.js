f9aff2aab0ebb32159b5db35a1a2ddb6533902ce
saiful millah <saeful@rubyh.co>
2021-06-05 02:59:43 +0700

    set facebook login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import GoogleLogin from 'react-google-login'
+import FacebookLogin from 'react-facebook-login'
 import axios from 'axios'
-import "bootstrap/dist/css/bootstrap.css";
-import FadeIn from "react-fade-in";
-import Lottie from "react-lottie";
-import * as legoData from "./legoLoading.json";
-import * as doneData from "./doneLoading.json";
-import * as errorData from "./errorLoading.json";
+import "bootstrap/dist/css/bootstrap.css"
+import FadeIn from "react-fade-in"
+import Lottie from "react-lottie"
+import * as legoData from "./legoLoading.json"
+import * as doneData from "./doneLoading.json"
+import * as errorData from "./errorLoading.json"
 
 
 const defaultOptions = {
@@ -38,26 +39,46 @@ const defaultOptions3 = {
 
 
 export default class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      start: undefined,
-      loading: undefined,
-      done: false
-    }
+  state = {
+    start: undefined,
+    loading: undefined,
+    done: false
   }
 
-  responseGoogle = (response) => {
-    if (response.tokenObj) {
+  responseFacebook = response => {
+    if (response.accessToken) {
       this.setState({ start: true })
-      const token = response.tokenObj.id_token
-      const provider = response.tokenObj.idpId
+      const token = response.accessToken
       axios
         .post(
           `${process.env.REACT_APP_MENUKU_URL}/api/auth/request`,
           {
             token: token,
-            provider: provider
+            provider: 'facebook'
+          }
+        )
+        .then((response) => {
+          this.setState({ loading: true, done: true })
+          console.log(response)
+        })
+        .catch((e) => {
+          this.setState({ loading: true })
+          console.log(e.response)
+          console.log("Response", e);
+        });
+    }
+  }
+
+  responseGoogle = response => {
+    if (response.tokenObj) {
+      this.setState({ start: true })
+      const token = response.tokenObj.id_token
+      axios
+        .post(
+          `${process.env.REACT_APP_MENUKU_URL}/api/auth/request`,
+          {
+            token: token,
+            provider: 'google'
           }
         )
         .then((response) => {
@@ -77,13 +98,25 @@ export default class App extends Component {
       <FadeIn>
         <div className="d-flex align-items-center justify-content-center mt-5">
           { !this.state.start ? (
-            <GoogleLogin
-              clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-              buttonText='Login with Google'
-              onSuccess={this.responseGoogle}
-              onFailure={this.responseGoogle}
-              cookiePolicy={'single_host_origin'}
-            />
+            <div>
+              <h1>LOGIN WITH FACEBOOK OR GOOGLE</h1>
+              <div className='facebook d-flex align-items-center justify-content-center mb-3 mt-5'>
+                <FacebookLogin
+                  appId={process.env.REACT_APP_FACEBOOK_APP_ID}
+                  callback={this.responseFacebook}
+                />
+              </div>
+              <div className='google d-flex align-items-center justify-content-center'>
+                <GoogleLogin
+                  clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                  buttonText='Login with Google'
+                  onSuccess={this.responseGoogle}
+                  onFailure={this.responseGoogle}
+                  cookiePolicy={'single_host_origin'}
+                />
+              </div>
+
+            </div>
           ) : (
               <div className="d-flex align-items-center justify-content-center ">
                 <h1>Exchanging Token . . . . </h1>
